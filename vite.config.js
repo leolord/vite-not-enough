26ab0ea4ca36fcf650d0e319fb1227c989aed861
wujiantao <wujiantao@bytedance.com>
2023-08-28 16:20:45 +0800

    fix: 🐛 append `less` code to different fils is ok

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from "vite";
-import path from 'path'
+import path from "path";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, 'dist-vite')
+    outDir: path.resolve(__dirname, "dist-vite"),
   },
   css: {
     preprocessorOptions: {
       less: {
         additionalData(content, filename) {
           if (filename.match(/pageA/)) {
-            return `@primaryColor: blue;` + content;
+            return `${content} @primaryColor: gray;`;
           } else {
-            return `@primaryColor: gray;` + content;
+            return `${content} @primaryColor: yellow;`;
           }
         },
       },
